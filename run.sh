96528db552c53f3d7e4d9315a2d1c314714f82c6
lukago <golebiewski96@gmail.com>
2018-03-10 19:14:58 +0100

    dequant, histogram fix

---
+++ run.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+#python z1.py --read data/p1.json  --save s0.bin --savetxt s.txt --bins 10 --operation x \ --oppath data/p12.json --spfq 10 --levels 16 --defq 100
+python z1.py --read data/p7.json  --save s0.bin --savetxt s.txt --bins 3 --spfq 10 --levels 16
--- z1.py
+++ z1.py
@@ -229,6 +229,33 @@ def avg_pow(sig):
     return sum(y * y for y in sig.y) / len(sig.y)
 
 
+def mse(sig, sig_origin):
+    s = 0
+    for i in range(len(sig.y)):
+        s += (sig_origin.y[i] - sig.y[i]) ** 2
+    return s / len(sig.y)
+
+
+def snr(sig, sig_origin):
+    s1 = sum(y * y for y in sig_origin.y)
+    s2 = 0
+    for i in range(len(sig.y)):
+        s2 += (sig_origin.y[i] - sig.y[i]) ** 2
+    return 10 * np.log10(s1 / s2)
+
+
+def psnr(sig, sig_origin):
+    return 10 * np.log10(max(sig_origin.y) / mse(sig, sig_origin))
+
+
+def md(sig, sig_origin):
+    dif = 0
+    for i in range(len(sig.x)):
+        if dif < abs(sig.y[0] - sig_origin.y[0]):
+            dif = abs(sig.y[0] - sig_origin.y[0])
+    return dif
+
+
 def print_stats(sig):
     sig = trunc(sig)
     noinf = np.inf not in sig.y
@@ -237,6 +264,13 @@ def print_stats(sig):
     print('Root mean square: ', rms(sig) if noinf else '+-inf')
     print('Variance:', variance(sig) if noinf else '+-inf')
     print('Average power: ', avg_pow(sig) if noinf else '+-inf')
+
+
+def print_stats_quant(sig, sig_origin):
+    print('MSE: ', mse(sig, sig_origin))
+    print('SNR: ', snr(sig, sig_origin), ' dB')
+    print('PSNR: ', psnr(sig, sig_origin), ' dB')
+    print('MD: ', md(sig, sig_origin))
 
 
 def read(path):
@@ -281,7 +315,7 @@ def sig_op(sig1, sig2, op):
         return sig_add(sig1, sig2)
     if op == '-':
         return sig_sub(sig1, sig2)
-    if op == '*':
+    if op == 'x':
         return sig_mul(sig1, sig2)
     if op == '/':
         return sig_div(sig1, sig2)
@@ -290,12 +324,14 @@ def sig_op(sig1, sig2, op):
 def trunc(sig):
     if len(sig.params_hist) == 1:
         p = sig.params_hist[0][1]
-        if p.duration > p.period:
-            x = np.arange(p.time_start, p.period + p.time_start, 1 / p.sampfq)
-            y = sig.y[:len(x)]
-            return Signal(x, y, sig.params_hist)
-    else:
-        return sig
+        if p.type == SignalType.SIN or p.type == SignalType.SIN_SINGLE \
+                or p.type == SignalType.SIN_DOUBLE or p.type == SignalType.RECT \
+                or p.type == SignalType.RECT_SIMETRIC or p.type == SignalType.TRI:
+            if p.duration > p.period:
+                x = np.arange(p.time_start, p.period + p.time_start, 1 / p.sampfq)
+                y = sig.y[:len(x)]
+                return Signal(x, y, sig.params_hist)
+    return sig
 
 
 def sample(sig, fq):
@@ -309,29 +345,46 @@ def sample(sig, fq):
     return Signal(x, y, sig.params_hist)
 
 
-def quant(sig, levels):
+def quant(sig, new_min, new_max, round_flag):
     old_min = min(sig.y)
     old_max = max(sig.y)
     old_range = np.abs(old_max - old_min)
-    step = old_range / levels
-    bins = np.arange(old_min, old_max, step)
-    y = []
-
-    for i in range(len(sig.y)):
-        best_match = (bins[0], (abs(bins[0]) - sig.y[i]))
-        for m in range(len(bins)):
-            dist = abs(bins[m] - sig.y[i])
-            if dist < best_match[1]:
-                best_match = (bins[m], dist)
-        y.append(best_match[0])
-
+    new_range = np.abs(new_max - new_min)
+    if round_flag:
+        y = np.round(((sig.y - old_min) * new_range / old_range) + new_min)
+    else:
+        y = np.round(((sig.y - old_min) * new_range / old_range) + new_min)
     return Signal(sig.x, y, sig.params_hist)
 
 
-def plot_sig(sig):
-    plt.plot(sig.x, sig.y, marker='o')
+def dequant_sinc(sig, new_fq):
+    x = np.arange(sig.x[0], sig.x[-1], 1 / new_fq)
+    y = []
+    peroid = sig.x[1] - sig.x[0]
+    for i in range(len(x)):
+        y.append(np.sum(sig.y * np.sinc((x[i] - sig.x) / peroid)))
+    return Signal(x, y, sig.params_hist)
+
+
+def plot_sig(sig, title='', show=True):
+    plt.plot(sig.x, sig.y, marker='.')
     plt.xlabel('t[s]')
     plt.ylabel('A')
+    plt.title(title)
+    if show:
+        plt.show()
+
+
+def plot_quant_signals(sig1, sig2, sig3, sig4):
+    plt.figure(1)
+    plt.subplot(221)
+    plot_sig(sig1, 'sygnal oryginalny', False)
+    plt.subplot(222)
+    plot_sig(sig2, 'sygnal sprobkowany', False)
+    plt.subplot(223)
+    plot_sig(sig3, 'sygnal skwantowany', False)
+    plt.subplot(224)
+    plot_sig(sig4, 'sygnal aproksymowany', False)
     plt.show()
 
 
@@ -348,17 +401,20 @@ def main():
     parser.add_argument('--read', help='path to json config or saved signal', default='data/p1.json')
     parser.add_argument('--save', help='path to save signal', default='s0.bin')
     parser.add_argument('--savetxt', help='path to save signal in txt', default='s.txt')
-    parser.add_argument('--bins', help='number of bins for histogram', default=10)
-    parser.add_argument('--operation', help='optional operation [+-/*] with signals')
+    parser.add_argument('--bins', help='number of bins for histogram', type=int, default=10)
+    parser.add_argument('--operation', help='optional operation [+-/x] with signals')
     parser.add_argument('--oppath', help='path to saved signal for operation')
+    parser.add_argument('--spfq', help='sampling fq', type=int)
+    parser.add_argument('--levels', help='quantization levels', type=int)
+    parser.add_argument('--defq', help='dequant fq, default is old fq', type=int)
 
     results = parser.parse_args()
     readpath = results.read
     savepath = results.save
     savetxtpath = results.savetxt
     bins = results.bins
-
     sig1 = read(readpath)
+
     if results.operation:
         op = results.operation
         oppath = results.oppath
@@ -369,12 +425,21 @@ def main():
     if savetxtpath:
         sig_to_txt(sig1, savetxtpath)
 
-    print_stats(sig1)
-    plot_sig(sig1)
-    sig2 = sample(sig1, 5)
-    plot_sig(sig2)
-    sig2 = quant(sig2, 5)
-    plot_sig(sig2)
+    # zad 2
+    spfq = results.spfq
+    levels = results.levels
+    defq = 1 / (sig1.x[1] - sig1.x[0])
+    if results.defq:
+        defq = results.defq
+
+    sig2 = sample(sig1, spfq)
+    sig3 = quant(sig2, 0, levels - 1, True)
+    sig4 = quant(sig3, min(sig1.y), max(sig1.y), False)
+    sig5 = dequant_sinc(sig4, defq)
+
+    print_stats_quant(sig5, sig1)
+    plot_quant_signals(sig1, sig2, sig3, sig5)
+    plot_hist(sig1, bins)
 
 
 if __name__ == "__main__":
